77a3dc2278d37c0783372da76b943e2b48ed14fa
Al Snow <jasnow@hotmail.com>
2023-07-10 17:17:03 -0400

    Initial 585-625 repo

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,12 @@ require 'kwalify'
 require 'rspec'
 
 ROOT = File.expand_path(File.join(__dir__,'..'))
+
+RSpec::Matchers.define :match_array_with_extra_elements do |expected|
+  match do |actual|
+    extra_elements = expected.each { |e| actual.delete(e) }
+    if extra_elements.empty?
+      true
+    end
+  end
+end
